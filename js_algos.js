8a9c492e61b8b780e9ea9cf8b2edb3cef03feb01
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2019-08-02 14:32:30 -0400

    added bubbleSort, factorial, subStr

--- js_algos.js
+++ js_algos.js
@@ -37,6 +37,63 @@ function reverse(str) {
 reverse('learning everyday');
 
 
+// sort any given array
+function bubbleSort(arr) {
+  let n = arr.length;
+  // let temp;
+    while(n > 1) {
+      for (let i = 0; i < n - 1; i++) {
+        if(arr[i] > arr[i + 1]) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+        }
+      }
+      n--;
+    }
+    return arr;
+}
+
+console.log(bubbleSort([99, 3, 7, 21, 33, 6, 43, 90, 7, 1]));
+
+
+// return the factorial of any given number
+function factorial(someNum) 
+{ 
+  if (someNum === 0)
+ {
+    return 1;
+ }
+  return someNum * factorial(someNum - 1);  
+}
+console.log(factorial(5));
+
+
+// return the substring of any given string
+function subStr(someStr, startIndex, endIndex) {
+  if (someStr.length === 0) {
+    return 'input string';
+   }
+  let subStrLength = endIndex - startIndex;
+  
+  if (subStrLength < 1) {
+    return 'string length must be more than one character in order to return a substring';
+  }
+  let subStr = '';
+  
+  if (endIndex > someStr.length - 1) {
+    subStrLength = someStr.length - startIndex;
+  }
+  
+  for (let i = 0; i < subStrLength; i++) {
+    subStr += someStr[i + startIndex];
+  }
+  return subStr;
+}
+
+let myString = 'hello!!';
+
+console.log(subStr(myString, 1, 6));
 
 
 
