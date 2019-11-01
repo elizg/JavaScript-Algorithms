be73f3ed747936349c4e93b423f087650a75ff5e
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2019-10-31 22:25:40 -0700

    added squareArrVals

--- js_algos.js
+++ js_algos.js
@@ -244,4 +244,12 @@ shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
 // output: (13) [2, 4, 6, 7, 13, 10, 8, 5, 11, 9, 1, 12, 3]
 
 
-
+// square each value in a given array
+function squareArrVals(arr){
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * arr[i]
+  }
+  return arr
+}
+squareArrVals([2, 3, 4, 5, 7, 9, 100, 500, 1200]);
+// output: (9) [4, 9, 16, 25, 49, 81, 10000, 250000, 1440000]
