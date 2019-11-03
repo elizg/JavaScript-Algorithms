ba27e0d52c1f1083ad59a30e1aa7c9704c07f7bd
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2019-11-03 13:48:13 -0800

    added minToFront

--- js_algos.js
+++ js_algos.js
@@ -335,3 +335,24 @@ function arrayMin(arr) {
   return min;
 };
 arrayMin([3,6,9,1]);
+
+
+
+// find the min val of an array and move it to idx 0
+function minToFront(arr){
+  let min = 0;
+  for (let i = 1; i < arr.length; i ++){
+      if (arr[i] < arr[min]){
+          min = i;
+      }
+  }
+  let temp = arr[min];
+  for (let i = min; i > 0; i--){
+      arr[i] = arr[i - 1];
+  }
+  arr[0] = temp;
+  return arr;
+}
+
+console.log(minToFront([99, 125, 32, 1, 24, 265, 3]));
+// output: (7) [1, 99, 125, 32, 24, 265, 3]
