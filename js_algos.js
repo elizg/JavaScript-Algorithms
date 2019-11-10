642a087ab8bee10a929670c76fd71c4361550ec3
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2019-11-10 12:23:19 -0800

    added removePunctuation

--- js_algos.js
+++ js_algos.js
@@ -396,3 +396,31 @@ function arrayMax(arr) {
 };
 arrayMax([1, 2, 3, 99, 5]);
 // output: 99
+
+
+
+// remove the punctuation from a given string
+function removePunctuation(str, len) {
+  if (typeof str == "string" && typeof len == "number") {
+      let s = "";
+      let start = str.length - 1;
+      let total = str.length;
+      while (total > len && start >= 0) {
+          if (str[start] == "." || str[start] == "," || str[start] == "!" || str[start] == "?" || str[start] == ";" || str[start] == ":" || str[start] == "-" || str[start] == "'") {
+              start--;
+              total--;
+          }
+          s += str[start];
+          start--;
+      }
+      for (let i = start; i >= 0; i--) {
+          s += str[i];
+      }
+      let reversedString = "";
+      for (let i = s.length - 1; i >= 0; i--) reversedString += s[i];
+      return reversedString;
+  }
+}
+removePunctuation("Hello, can you remove the punctuation? Thank you!", 1);
+// output: "Hello can you remove the punctuation Thank you"
+// TODO review removePunctuation
