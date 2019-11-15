a59d6e263abe44a06cfdbdb2bb8d9d69b42294f0
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2019-11-14 20:52:14 -0800

    added removeNeg

--- js_algos.js
+++ js_algos.js
@@ -456,3 +456,23 @@ function secondToLast(arr) {
 }
 secondToLast([1, 2, 3, 4, 5, 6, 7]);
 // output: 6
+
+
+
+// remove the negative numbers from an array
+function removeNeg(arr) {
+  let position = 0;
+  for(let i = 0; i < arr.length; i++) {
+      if(arr[i] < 0) {
+          continue;
+      }
+      else {
+          arr[position] = arr[i];
+          position++;
+      }
+  }
+  arr.length = position;
+  return arr;
+}
+removeNeg([-99, -25, -4, 0, 1, 2, 3, 4, 5, -700]);
+// output: (6) [0, 1, 2, 3, 4, 5]
