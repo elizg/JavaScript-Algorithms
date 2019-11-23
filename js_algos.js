e1df842a26fc72e9b88f6c29106589098c061675
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2019-11-23 14:01:52 -0800

    added LinkedList

--- js_algos.js
+++ js_algos.js
@@ -476,3 +476,60 @@ function removeNeg(arr) {
 }
 removeNeg([-99, -25, -4, 0, 1, 2, 3, 4, 5, -700]);
 // output: (6) [0, 1, 2, 3, 4, 5]
+
+
+
+
+// construct linked list and find the average of added node values
+class LinkedList {
+  constructor() {
+      this.head = null;
+  }
+  
+  // Adds a value to the linked list @param {Object} val 
+  add(val) {
+      var lastNode = this.head;
+      var addNode = new node(val);
+      this.head = addNode;
+      addNode.next = lastNode;
+      if (this.back == null) this.back = addNode;
+      return this;
+  }
+
+  // returns a formatted string of the list
+  toString() {
+      let node = this.head;
+      let s = "[ "
+      while (node != null) {
+          if (node.next == null) break;
+          s += node.val + ", ";
+          node = node.next;
+      }
+      s += node.val + " ]";
+      return s;
+  }
+
+  // returns average value of all the nodes
+  average() {
+      let node = this.head
+      let length = 0, sum = 0;
+      while (node != null) {
+          length++;
+          sum += node.val;
+          node = node.next;
+      }
+      return sum / length;
+  }
+}
+
+class node {
+  constructor(val) {
+      this.val = val;
+      this.next = null;
+  }
+}
+
+let list = new LinkedList();
+list.add(3).add(55).add(300);
+console.log(list.average());
+// output: 119.33333333333333
